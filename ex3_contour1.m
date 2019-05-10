cc90190c23951f8e5f44fde4779e664e4f5e07b3
Marcin-Bak1 <kontakt.marcin.bak@gmail.com>
2019-05-10 12:16:17 +0100

    Exercise 3 for week1

---
+++ ex3_contour1.m
@@ -0,0 +1,36 @@
+xmin = 0;
+xmax = 5;
+nx = 51;
+ymin = 0;
+ymax = 4;
+ny = 41;
+del = 1.5;
+nv = 100;
+xa = 4.1;
+ya = 1.3;
+xb = 2.2;
+yb = 2.9;
+for i = 1:nx
+    
+    for j = 1:ny
+        
+        %Defining the grid points
+        X(i,j) = xmin  + (i-1) * (xmax-xmin)/(nx-1);
+        Y(i,j) = ymin  + (j-1) * (ymax-ymin)/(ny-1);
+        %Calculating infa and infb using the external function
+        [infa(i,j), infb(i,j)] = panelinf(del, xa, ya, xb, yb, X(i,j), Y(i,j));
+        
+    end
+    
+end
+%c = -0.15:0.05:0.15;
+
+figure(1)
+contourf(X, Y,infa)
+title('infa formula - general sheet')
+colorbar
+
+figure(2)
+contourf(X, Y,infb)
+title('infb formula - general sheet')
+colorbar
