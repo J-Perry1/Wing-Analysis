05ffd769eb2d61eca39d54c2c12b57bc4faf3032
Marcin-Bak1 <kontakt.marcin.bak@gmail.com>
2019-05-10 13:26:38 +0100

    Eliminating tip errors from ex2 finite vortex approximation

--- ex2_contour1.m
+++ ex2_contour1.m
@@ -2,10 +2,6 @@
 %This script plots influence coefficients a and b for the vortices from the
 %expressions derived in the notes as well as approximates it using the
 %finite vortex sheet small vortex contributions
-
-close all 
-clear all
-
 xmin = -2.5;
 xmax = 2.5;
 nx = 51;
@@ -32,7 +28,12 @@ for i = 1:nx
             L = k*(del/nv);
             gamx = 1 - k * (1/nv); %vulnerable to error
             gam = gamx * (del/nv);
-            tempor_psi = psipv(L, 0, gam, X(i,j), Y(i,j));
+            if abs(Y(i,j)) < 1e-19
+                Yin = 1e-19;
+            else
+                Yin = Y(i,j);
+            end
+            tempor_psi = psipv(L, 0, gam, X(i,j), Yin);
             psi_a(i,j) = psi_a(i,j) + tempor_psi;
             
         end
@@ -42,7 +43,12 @@ for i = 1:nx
             L = k*del/nv;
             gamx = k * (1/nv); %vulnerable to error
             gam = gamx * (del/nv);
-            tempor_psi = psipv(L, 0, gam, X(i,j), Y(i,j));
+            if abs(Y(i,j)) < 1e-19
+                Yin = 1e-19;
+            else
+                Yin = Y(i,j);
+            end
+            tempor_psi = psipv(L, 0, gam, X(i,j), Yin);
             psi_b(i,j) = psi_b(i,j) + tempor_psi;
         end
         
@@ -53,20 +59,20 @@ c = -0.15:0.05:0.15;
 
 figure(1)
 contourf(X, Y,infa,c)
-%title('infa formula')
+title('infa formula')
 colorbar
 
 figure(2)
 contourf(X, Y,psi_a, c)
-%title('infa discrete vortex approximation')
+title('infa discrete vortex approximation')
 colorbar
 
 figure(3)
 contourf(X, Y,infb, c)
-%title('infb formula')
+title('infb formula')
 colorbar
 
 figure(4)
 contourf(X, Y,psi_b, c);
-%title('infb discrete vortex approximation')
+title('infb discrete vortex approximation')
 colorbar
