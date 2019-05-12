67558031905861368ee3fb6c217614b64ad4181f
Marcin-Bak1 <kontakt.marcin.bak@gmail.com>
2019-05-12 21:12:34 +0100

    Ex4 working version

--- Ex4_contour.m
+++ Ex4_contour.m
@@ -1,3 +1,5 @@
+clear all
+close all
 np = 100;
 nx = 51;
 ny = 41;
@@ -8,7 +10,7 @@ ymax = 2;
 theta = (0:np)*2*pi/np;
 xs = cos(theta);
 ys = sin(theta);
-gammac = -2sin(theta);
+gammac = -2*sin(theta);
 
 for i = 1:nx
     
@@ -18,7 +20,24 @@ for i = 1:nx
         X(i,j) = xmin  + (i-1) * (xmax-xmin)/(nx-1);
         Y(i,j) = ymin  + (j-1) * (ymax-ymin)/(ny-1);
         
-        for 
+        psi(i,j) = Y(i,j);
         
+        for k = 1:np
+            
+            [infa, infb] = panelinf(xs(k), ys(k), xs(k+1), ys(k+1), X(i,j), Y(i,j));
+            tempor = infa * gammac(k) + infb * gammac(k+1);
+            psi(i,j) = psi(i,j) + tempor;
         end
     end
+    
+end
+    
+c = -1.75:0.25:1.75;
+
+figure(1)
+contour(X,Y, psi, c)
+colorbar
+
+hold on
+plot(xs,ys)
+hold off
