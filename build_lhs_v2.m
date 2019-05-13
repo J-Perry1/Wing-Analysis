2abc8fd8f3132cb2c877d24fd0045af5244c1b05
Marcin-Bak1 <kontakt.marcin.bak@gmail.com>
2019-05-13 10:56:28 +0100

    Version 2 of the lhs

---
+++ build_lhs_v2.m
@@ -0,0 +1,32 @@
+function lhsmat = build_lhs_v2(xs,ys)
+
+np = length(xs) - 1;
+disp(np)
+psip = zeros(np, np+1);
+A = zeros(np+1, np+1);
+
+for i = 1:np
+    
+    j = 1;
+    [infaj , infbj] = panelinf(xs(j), ys(j), xs(j+1), ys(j+1), xs(i), ys(i));
+    psip(i,j) = infaj;
+    for j = 2:np
+        [infaj_1, infbj_1] = panelinf(xs(j-1), ys(j-1), xs(j), ys(j), xs(i), ys(i));
+        [infaj , infbj] = panelinf(xs(j), ys(j), xs(j+1), ys(j+1), xs(i), ys(i));
+        psip(i,j) = infaj + infbj_1;
+  
+   
+    end
+    j = np + 1;
+    [infaj_1, infbj_1] = panelinf(xs(j-1), ys(j-1), xs(j), ys(j), xs(i), ys(i));
+    psip(i,j) = infbj_1;
+    
+end
+
+
+for i = 1:np+1
+    A(i,:) = psip(i+1,:) - psip(i,:);
+end
+
+lhsmat = A;
+end
