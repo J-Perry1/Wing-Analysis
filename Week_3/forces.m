bbb91695fc93fc56b53b46b09e37fb446ce5ed8a
John Perry <jajp2@cam.ac.uk>
2019-05-27 09:26:46 +0100

    changed function to consider average values of upper and lower surfaces

--- Week_3/forces.m
+++ Week_3/forces.m
@@ -1,9 +1,9 @@
 function [cl,cd] = forces(circ,cp,delstarl,thetal,delstaru,thetau)
 %Calculates the lift and drag coefficients for the aerofoil
 
-thetate = thetal(length(theatal)); %use upper or lower or average?
+thetate = 0.5*(thetal(length(theatal))+thetau(length(theatau))); %use average?
 uete = sqrt(1-cp(length(cp)); %from equation on p27;
-Hte = delstarl(length(delstarl))/thetal(length(thetal));
+Hte = 0.5*(delstarl(length(delstarl))/thetal(length(thetal)) + delstaru(length(delstaru))/thetau(length(thetau)));
 thetainf = thetate*(uete)^(0.5*(Hte+5));
 
 cd = 2*thetainf;
