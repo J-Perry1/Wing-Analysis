3684dc22db560d9007e9a0c7610a2b6601cf0c27
John Perry <jajp2@cam.ac.uk>
2019-05-27 14:19:27 +0100

    changes made to forces calculator for calculating drag coeff

--- Week_3/forces.m
+++ Week_3/forces.m
@@ -1,9 +1,9 @@
 function [Cl,Cd] = forces(circ,cp,delstarl,thetal,delstaru,thetau)
 %Calculates the lift and drag coefficients for the aerofoil
 
-thetate = 0.5*(thetal(length(thetal))+thetau(length(thetau))); %use average?
-uete = sqrt(1-cp(length(cp))); %from equation on p27;
-Hte = 0.5*(delstarl(length(delstarl))/thetal(length(thetal)) + delstaru(length(delstaru))/thetau(length(thetau)));
+thetate = thetal(end)+thetau(end); %use average?
+uete = sqrt(1-cp(end)); %from equation on p27;
+Hte = (delstarl(end) + delstaru(end))/thetate;
 thetainf = thetate*(uete)^(0.5*(Hte+5));
 
 Cd = 2*thetainf;
