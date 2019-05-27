a09c19d1ddf444afa3449f8e10d061b328a73593
John Perry <jajp2@cam.ac.uk>
2019-05-27 09:52:38 +0100

    files needed to run foil programme

--- Week_3/forces.m
+++ Week_3/forces.m
@@ -1,12 +1,12 @@
-function [cl,cd] = forces(circ,cp,delstarl,thetal,delstaru,thetau)
+function [Cl,Cd] = forces(circ,cp,delstarl,thetal,delstaru,thetau)
 %Calculates the lift and drag coefficients for the aerofoil
 
-thetate = 0.5*(thetal(length(theatal))+thetau(length(theatau))); %use average?
-uete = sqrt(1-cp(length(cp)); %from equation on p27;
+thetate = 0.5*(thetal(length(thetal))+thetau(length(thetau))); %use average?
+uete = sqrt(1-cp(length(cp))); %from equation on p27;
 Hte = 0.5*(delstarl(length(delstarl))/thetal(length(thetal)) + delstaru(length(delstaru))/thetau(length(thetau)));
 thetainf = thetate*(uete)^(0.5*(Hte+5));
 
-cd = 2*thetainf;
-cl = -2*circ;
+Cd = 2*thetainf;
+Cl = -2*circ;
 end
 
